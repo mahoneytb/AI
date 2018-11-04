013a139a4d2c4d03b9e72be1819ea526a7266628
s42360366 <timothy.mahoney@uq.edu.au>
2018-11-04 22:07:25 +1000

    could this be finished?

--- Assignment2/src/problem/Main.java
+++ Assignment2/src/problem/Main.java
@@ -16,26 +16,27 @@ public class Main {
         try {
             double wins = 0;
             int steps = 0;
-            for (int i=0; i<100; i++) {
-                String filename = "examples/level_5/input_lvl5_" + i + ".txt";
-                ps = new ProblemSpec(filename);
-                Simulator simulator = new Simulator(filename, "lvl5Out");
-                Simulate testSimulator = new Simulate(ps);
-                System.out.println(ps.toString());
-                MCTS MCTS = new MCTS(ps, testSimulator);
+            //for (int i=0; i<100; i++) {
+                //String filename = "examples/level_5/input_lvl5_" + i + ".txt";
+            String filename = "examples/level_3/input_lvl3_" + 0 + ".txt";
+            ps = new ProblemSpec(filename);
+            Simulator simulator = new Simulator(filename, "lvl5Out");
+            Simulate testSimulator = new Simulate(ps);
+            System.out.println(ps.toString());
+            MCTS MCTS = new MCTS(ps, testSimulator);
 
 
-                while (simulator.getSteps() <= ps.getMaxT()) {
-                    Action action = MCTS.run();
-                    State nextState = simulator.step(action);
-                    MCTS = new MCTS(ps, testSimulator, nextState, simulator.getSteps(), action);
-                    if (nextState.getPos() >= ps.getN()) {
-                        wins++;
-                        steps += simulator.getSteps();
-                        break;
-                    }
+            while (simulator.getSteps() <= ps.getMaxT()) {
+                Action action = MCTS.run();
+                State nextState = simulator.step(action);
+                MCTS = new MCTS(ps, testSimulator, nextState, simulator.getSteps(), action);
+                if (nextState.getPos() >= ps.getN()) {
+                    wins++;
+                    steps += simulator.getSteps();
+                    break;
                 }
             }
+            //}
             System.out.println(wins + " out of 100");
             System.out.println(steps/wins + " steps on average");
         } catch (IOException e) {
